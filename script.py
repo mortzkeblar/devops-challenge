f545d3fcfd98a6316abda78798bc43d3ae11be0c
Marco Canaza <mortzkeb@gmail.com>
2024-01-18 02:03:26 -0300

    mod: use docker sdk in python

--- script.py
+++ script.py
@@ -1,33 +1,63 @@
 #!/usr/bin/env python3
 
 import subprocess
-
-image = 'devops-challenge:latest'
-db_dir = '/tmp/db'
+import docker
 
 
-def create_image():
-    subprocess.run(f'docker build -t {image} .', shell=True)
-    print(subprocess.run(f"docker images {image}", shell=True, stdout=subprocess.PIPE, text=True).stdout)
+def print_message(message):
+    print(f"{'=' * 40}\n{message}\n{'=' * 40}")
 
 
-def run_container():
-    if no_clone():
-        subprocess.run(f'docker run -d -v {db_dir}:/src/instance {image}', shell=True)
+def check_exists_image(dock_image):
+    client = docker.from_env()
+    try:
+        client.images.get(dock_image)
+        return True
+    except docker.errors.ImageNotFound:
+        return False
 
 
-# {db_dir}/db.sqlite3/:/src/instance/db.sqlite3
+def is_containers_active(dock_image):
+    client = docker.from_env()
+    containers = client.containers.list(all=True)
 
-def no_clone():
-    listas = subprocess.run("docker ps -a | awk '{print $2}' | tail -n +2 ", shell=True, stdout=subprocess.PIPE,
-                            text=True).stdout.split("\n")
-    for lista in listas:
-        if lista == image:
-            print("Ya existe una un contenedor en ejecución")
-            return False
+    if not containers:
+        print("Container status: not containers created")
+        return
+
+    for container in containers:
+        print(container.attrs['Config']['Image'])
+        if dock_image == container.attrs['Config']['Image']:
+            if container.status == "running":
+                print(f"Container status: running")
+            else:
+                print(f"Container status: exited")
 
 
-create_image()
-#run_container()
+def show_image(dock_image):
+    client = docker.from_env()
+    image_show = client.images.get(dock_image)
+    print(f"Image name: {dock_image}")
+    print(f"Image ID: {image_show.short_id}")
+    print(f"Created: {image_show.attrs['Created']}")
+    is_containers_active(dock_image)
 
-#no_clone()
+
+def create_image(dock_image):
+    if check_exists_image(dock_image):
+        print_message(f"The image {dock_image} already exists")
+        show_image(image)
+        return
+
+    subprocess.run(f'docker build -t {dock_image} .', shell=True)
+    print_message(f"The image {dock_image} created")
+    show_image(image)
+
+
+if __name__ == "__main__":
+    image_name = "devops-challenge"
+    image_tag = "latest"
+    image = image_name + ":" + image_tag
+
+    create_image(image)
+    # is_containers_active(image_name)
