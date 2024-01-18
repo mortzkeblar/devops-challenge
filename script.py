1c4c66ae9e7ef6793a2017dd122e6d694afffd32
Marco Canaza <mortzkeb@gmail.com>
2024-01-18 03:21:09 -0300

    add: delete and parser functions in py script

--- script.py
+++ script.py
@@ -2,10 +2,18 @@
 
 import subprocess
 import docker
+import argparse
 
 
 def print_message(message):
     print(f"{'=' * 40}\n{message}\n{'=' * 40}")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Script for managing the file upload/download microservice")
+    parser.add_argument("-fi", "--forceimage", action="store_true", help="Forzar la construcción de la imagen")
+    parser.add_argument("-rmi", "--rmimage", action="store_true", help="Mostrar información de la imagen")
+    return parser.parse_args()
 
 
 def check_exists_image(dock_image):
@@ -43,21 +51,42 @@ def show_image(dock_image):
     is_containers_active(dock_image)
 
 
-def create_image(dock_image):
+def delete_image(dock_image):
+    client = docker.from_env()
+    client.images.remove(dock_image)
+    print_message(f"{dock_image} ha sido borrado")
+
+
+def create_image(dock_image, force):
     if check_exists_image(dock_image):
+        if force:
+            subprocess.run(f'docker build --no-cache -t {dock_image} .', shell=True)
+            print_message(f"The image {dock_image} recreated")
+            show_image(dock_image)
+            return
+
         print_message(f"The image {dock_image} already exists")
-        show_image(image)
+        show_image(dock_image)
         return
 
     subprocess.run(f'docker build -t {dock_image} .', shell=True)
     print_message(f"The image {dock_image} created")
-    show_image(image)
+    show_image(dock_image)
 
 
-if __name__ == "__main__":
+def main():
     image_name = "devops-challenge"
     image_tag = "latest"
     image = image_name + ":" + image_tag
 
-    create_image(image)
+    args = parse_arguments()
+    if args.rmimage:
+        delete_image(image)
+        return
+
+    create_image(image, args.forceimage)
     # is_containers_active(image_name)
+
+
+if __name__ == "__main__":
+    main()
